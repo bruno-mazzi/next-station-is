351f77186178e9474470d23d54a9acc96957276b
unknown <houille@yahoo.fr>
2019-12-29 01:03:42 +0100

    ajoutfavicon et suite slider avec son initialisation

--- home-station/js/script.js
+++ home-station/js/script.js
@@ -1,3 +1,39 @@
+var listBG = [
+    {
+        nameJp: '屋久島',
+        nameOc: 'Yakushima',
+        img: 'yakushima.jpg'
+    },
+    {
+        nameJp: '彦根城',
+        nameOc: 'Château d\'Hikone',
+        img: 'hikone_castle.jpg'
+    },
+    {
+        nameJp: '礼文島',
+        nameOc: 'île Rebun',
+        img: 'rebun.jpg'
+    }
+]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function initBG() {
+    var numBG = getRandomInt(listBG.length);
+    var numBGD = numBG === (listBG.length-1) ? 0 : (numBG + 1);
+    var body = document.getElementsByTagName("body")[0];
+    for (var i = 0; i < 3; i++) {
+        if (numBG === listBG.length) {
+            numBG = 0;
+        }
+        document.getElementById("js-njp"+i).textContent= listBG[numBG].nameJp;
+        document.getElementById("js-noc"+i).textContent= listBG[numBG].nameOc;
+        numBG++;
+    }
+    body.style.backgroundImage = "url(images/"+listBG[numBGD].img+")";
+}
 
 function changeBG(e) {
     var body = document.getElementsByTagName("body")[0];
@@ -10,6 +46,8 @@ function changeBG(e) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+    initBG();
+
     var elePrev = document.getElementsByClassName('js-carrousel-prev');
     var eleNext = document.getElementsByClassName('js-carrousel-next');
 
